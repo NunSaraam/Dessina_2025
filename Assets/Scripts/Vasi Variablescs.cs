5825e8b6b735cdfd6e839ec0b5eb43a5c84d9118
NunSaraam <yjc22950037@gmail.com>
2025-03-18 18:07:13 +0900

    [Feat] 조건문과 키 입력을 통한 오브젝트의 이동

--- Assets/Scripts/Vasi Variablescs.cs
+++ Assets/Scripts/Vasi Variablescs.cs
@@ -13,12 +13,36 @@ public class VasiVariablescs : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("게임시작");                  //유니티 디버그에 메시지 출력
+        //Debug.Log("게임시작");                  //유니티 디버그에 메시지 출력
+        // == 두 값이 같을 때 true
+        // != 두 값이 다르면 true
+        // > 왼쪽 값이 더 크면 true
+        // < 오른쪽 값이 더 크면 true
+        // >= 왼쪽 값이 크거나 같으면 true
+        // <= 오른쪽 값이 크거나 같으면 true
+
+        if (gold > 50)
+        {
+            Debug.Log("아이템을 구매할 수 있습니다.");
+        }
+        else if ( gold == 40 )  //if안에 조건이 참이 아닐 때 이 조건이 참이려면
+        {
+            Debug.Log("골드가 40원 보유 중 입니다. 10 골드만 더 모으면 살 수 있어요!");
+        }
+        //else  //if 안의 조건이 참이 아닐 때
+        //{
+        //  Debug.Log("골드가 부족합니다.");
+        //} 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("게임 진행중");
+        //Debug.Log("게임 진행중");
+        if(Input.GetKeyDown(KeyCode.Space))         //space를 눌렀을 때 true
+        {
+            gold = gold + 10;                       //gold에 10을 추가한다.
+            Debug.Log("현재 골드 : "  +  gold);     //유니티 콘솔에 현재 gold값을 띄워준다.
+        }
     }
 }
